3522d0a7bb1cff80e38d827baf1c241431c41175
m1zole <minrei728@gmail.com>
2024-02-08 22:48:55 +0900

    12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "PatchfinderUtils",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v12),
         .macOS(.v11)
     ],
     products: [
